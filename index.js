e4beff9ace6a76312d5cd3a4f3103ee7e855b939
zengxingqi <me@zengxingqi.com>
2020-03-15 22:36:07 +0800

    delete: log

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 const fringerprint = require("bindings")("fringerprint");
 // const fringerprint = require("./build/Release/fringerprint");
-console.log(fringerprint)
+
 module.exports = fringerprint;
